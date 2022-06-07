5ab5ad26f49129288b508d02e44baaf469bf0019
jungkeunmo <abc59684495@gmail.com>
2022-06-07 22:12:41 +0900

    SQLD_P

--- 자격증반 연습 3회 (SELECT : WHERE).sql
+++ 자격증반 연습 3회 (SELECT : WHERE).sql
@@ -50,7 +50,7 @@ SELECT	EMPNO,
 		ENAME,
         SAL
   FROM	emp
- WHERE 	EMPNO != ANY(7566, 7782, 7934);
+ WHERE 	EMPNO IN(7369, 7499, 7521, 7654, 7698, 7788, 7839, 7844, 7876, 7900, 7902);
  
 -- 부서번호 30(deptno)에서 근무하며
 -- 월2,000달러 이하를 받는 81년05월01일 이전에 입사한 사원의
@@ -69,8 +69,24 @@ SELECT	ENAME,
 		SAL,
         DEPTNO
   FROM 	emp
- WHERE  (SAL BETWEEN 2000 AND 5000) AND DEPTNO = 10; 
+ WHERE  (SAL BETWEEN 2000 AND 5000) AND DEPTNO IN (10, 30); 
+ 
+-- 업무가 SALESMAN 또는 MANAGER 이면서 급여가 1,600, 2,975 또는 2,850이 아닌
+-- 모든 사원의 이름, 업무 및 급여를 표시하시오.
+SELECT 	ENAME,
+		JOB,
+        SAL
+  FROM 	emp
+ WHERE 	JOB = 'SALESMAN'; 
 
+-- emp 테이블에서 사원이름 중 S가 포함되지 않는 사람들 중 부서번호가 20인 사원들의 
+-- 이름과 부서번호를 출력하세요.
+
+-- emp테이블에서 이름에 A와 E가 있는 모든 사원의 이름을 표시하시오.
+
+-- emp테이블에서 관리자가 없는 모든 사원의 이름과 업무를 표시하시오.
+
+-- emp테이블에서 커미션 항목이 입력된 사원들의 이름과 급여, 커미션을 구하시오.
 
 
 
